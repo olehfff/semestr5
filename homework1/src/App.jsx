7b15a449dc5b33222e2a7abdf2ffd0356c1447ab
olehfff <olegsamilenko5@gmail.com>
2025-04-29 19:56:42 +0300

    homework1

---
+++ homework1/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import reactIcon from './assets/react.svg'
+
+function App() {
+  return (
+    <>
+      <h1>
+       Олег
+      </h1>
+      <p>Ласкаво просимо до нашого сайту!</p>
+      <img src={reactIcon} alt="react" />
+      <a href="https://chatgpt.com/">Google</a>
+  
+    </>
+  )
+}
+
+export default App
